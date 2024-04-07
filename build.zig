919dd82003e51ba7893f11410a438e964d72eb10
savfrom4 <savfrom4@gmail.com>
2024-04-07 18:59:15 +0200

    Fixed build.zig

--- build.zig
+++ build.zig
@@ -4,28 +4,37 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "duktape-zig",
-        .root_source_file = .{ .path = "src/root.zig" },
+    // Static C library
+    const clib = b.addStaticLibrary(.{
+        .name = "libduktape",
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
 
-    lib.addCSourceFile(.{
+    clib.addCSourceFile(.{
         .file = .{ .cwd_relative = "./duktape-2.7.0/duktape.c" },
         .flags = &[_][]const u8{"-fno-sanitize=undefined"},
     });
-    lib.addIncludePath(.{ .cwd_relative = "./duktape-2.7.0/" });
-    b.installArtifact(lib);
+    clib.addIncludePath(.{ .cwd_relative = "./duktape-2.7.0/" });
+    b.installArtifact(clib);
 
+    // Main zig module
+    const module = b.addModule("duktape", .{
+        .root_source_file = .{ .path = "src/root.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    module.linkLibrary(clib);
+
+    // Unit tests
     const lib_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/root.zig" },
         .target = target,
         .optimize = optimize,
-        .link_libc = true,
     });
-    lib_unit_tests.linkLibrary(lib);
+    lib_unit_tests.linkLibrary(clib);
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
